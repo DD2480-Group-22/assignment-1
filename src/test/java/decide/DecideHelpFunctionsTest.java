e9672602328ed5f89982b5295df13996d484a25b
Nils Streijffert <nils.streijffert@gmail.com>
2020-01-25 13:22:04 +0100

    feat/#29 Add the seventh condition function

--- src/test/java/decide/DecideHelpFunctionsTest.java
+++ src/test/java/decide/DecideHelpFunctionsTest.java
@@ -1,9 +1,9 @@
 package decide;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DecideHelpFunctionsTest {
 
@@ -39,9 +39,53 @@ class DecideHelpFunctionsTest {
     void conditionFunctionSix() {
     }
 
-    @Test
-    void conditionFunctionSeven() {
-    }
+    @Nested
+    @DisplayName("Tests of the seventh condition function")
+    class ConditionFunctionSevenTests {
+        @Test
+        @DisplayName("Test function with input that should evaluate to true, evan kPts")
+        void correctInputEvan() {
+            double[][] array = {{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {2.0, 0.0}};
+            assertTrue(DecideHelpFunctions.conditionFunctionSeven(2, 1.0, array));
+        }
+
+        @Test
+        @DisplayName("Test function with input that should evaluate to true, odd kPts")
+        void correctInputOdd() {
+            double[][] array = {{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {2.0, 0.0}};
+            assertTrue(DecideHelpFunctions.conditionFunctionSeven(3, 1.0, array));
+        }
+
+        @Test
+        @DisplayName("Test function with large array that should evaluate to true")
+        void correctInputLongArray() {
+            double[][] array = {{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0},
+                    {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0},
+                    {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {2.0, 0.0}};
+            assertTrue(DecideHelpFunctions.conditionFunctionSeven(2, 1.0, array));
+        }
+
+        @Test
+        @DisplayName("Not enough data points")
+        void notEnoughPoints() {
+            double[][] array = {{0.0, 0.0}, {0.0, 0.0}};
+            assertFalse(DecideHelpFunctions.conditionFunctionSeven(2, 1.0, array));
+        }
+
+        @Test
+        @DisplayName("To short distance between points")
+        void incorrectDistance() {
+            double[][] array = {{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {7, 0}};
+            assertFalse(DecideHelpFunctions.conditionFunctionSeven(2, 7.0, array));
+        }
+
+        @Test
+        @DisplayName("Incorrect spacing between points")
+        void incorrectSpacing() {
+            double[][] array = {{0.0, 0.0}, {0.0, 0.0}, {7, 0}};
+            assertFalse(DecideHelpFunctions.conditionFunctionSeven(3, 7.0, array));
+        }
+   }
 
     @Test
     void conditionFunctionEight() {
